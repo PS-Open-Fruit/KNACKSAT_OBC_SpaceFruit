cc1e99bc5d3c914ad0393af7c7fb5a4e0d2de960
bipoe <bipoeman12@gmail.com>
2026-03-21 20:52:06 +0700

    ignore sync for venv

--- unison_sync.sh
+++ unison_sync.sh
@@ -1,1 +1,4 @@
-unison ./eps_and_payload_emulator/kiss_file_transfer/ ssh://bipoe@100.1.1.1//home//bipoe//payload_emulator// -repeat 2 -batch
+unison ./eps_and_payload_emulator/kiss_file_transfer/ ssh://bipoe@100.1.1.1//home//bipoe//payload_emulator// \
+    -repeat 2 \
+    -batch \
+    -ignore 'Name .venv'
